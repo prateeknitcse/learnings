c80460a98365e7a1ceee97039e0a87e72db3efaf
prateeksingh <prateeksingh010101@gmail.com>
2026-01-31 12:06:03 +0530

    made routes

--- urlshortner/controllers/url.js
+++ urlshortner/controllers/url.js
@@ -1,4 +1,18 @@
-async function generateShortUrl(req, res) {
+const { nanoid } = require('nanoid');
+const Url = require('../models/url');
+async function generateShortUrl(req, res){
+   const body = req.body;
+   if(!body.url) return res.status(400).json({ error: "URL is required" });
+    const shortID = nanoid(8);
+    await Url.create({
+        shortId: shortID,
+       redirectUrl: body.url,
+       visitHistory: [],
+    });
+
+   return res.json({id : shortID})
+
 
     
 }
+module.exports = {generateShortUrl};
--- urlshortner/routes/url.js
+++ urlshortner/routes/url.js
@@ -1,4 +1,5 @@
 const express = require('express');
  const router = express.Router();
+ const { generateShortUrl } = require('../controllers/url');
 
- router.post('/', (req, res) => {
+ router.post('/', generateShortUrl);
